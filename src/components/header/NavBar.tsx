08d93b68d886c74cf3d48956b28b667737523601
Hasib Hossain <boraborhasib@gmail.com>
2023-10-20 22:26:16 +0600

    Navbar work started

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import logo from '@/assets/logo.svg'; // 👈 import logo
+import Image from 'next/image';
+import { BiSearch } from 'react-icons/bi';
 
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='max-w-screen-xl mx-auto py-3'>
+            <div className="flex items-center ">
+                <div className="">
+                    <Image className='w-[200px]' width={300} height={150} src={logo} alt="logo" />
+                </div>
+                <div className="">
+                    <div className="relative">
+                        <input
+                            className=' outline-none rounded-full p-2 '
+                            type="text"
+                            placeholder='Search here...' />
+                        <BiSearch className='text-2xl  absolute top-2 right-3 ' />
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
