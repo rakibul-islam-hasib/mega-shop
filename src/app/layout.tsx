5296179339e85c9d213b6725e961b82f4d984059
Hasib Hossain <boraborhasib@gmail.com>
2023-10-20 11:06:39 +0600

    Init Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css'
+import { Bebas_Neue } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] })
+const bebasNeue = Bebas_Neue({ subsets: ['latin'], weight: ['400' as const], variable: '--bebas-font' })
+export const metadata: Metadata = {
+  title: 'Hotel Booking App',
+  description: 'By Rakibul Islam',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${bebasNeue.variable}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
