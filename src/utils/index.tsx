7632273839b0f6d8b8faac267ceed04ac58230c8
Hasib Hossain <boraborhasib@gmail.com>
2023-10-20 23:12:06 +0600

    more data added

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,41 @@
-export const topProducts: object[] = [
+// TYPES and INTERFACES
+type Product = {
+    _id: number;
+    name: string;
+};
+
+
+
+// DATA and EXPORTS
+
+export const topProducts: Product[] = [
+    {
+        _id: 1,
+        name: 'Iphone 12 Pro Max',
+    },
+    {
+        _id: 2,
+        name: 'GT 1080TI',
+    },
+    {
+        _id: 3,
+        name: 'Macbook Pro',
+    },
+    {
+        _id: 4,
+        name: 'Xbox Series X'
+    },
+    {
+        _id: 5,
+        name: 'Laptop Dell'
+    },
+    {
+        _id: 6,
+        name: 'Galaxy S21 Ultra'
+    },
+    {
+        _id: 7,
+        name: 'Acer Nitro 5'
+    }
 
 ];
